068e402f08ca8ea471876de70daeae681201d771
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-02-24 11:40:15 +0700

    update folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod routes;
+mod startup;
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
