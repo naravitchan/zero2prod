dabb2435bece754367c7b0338db7dbdd1a48225b
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-02-28 01:50:43 +0700

    log

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,29 +9,51 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    pool: web::Data<PgPool>, // Renamed!
-) -> HttpResponse {
-    println!("query");
-    match sqlx::query!(
-        "
-    INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
-    ",
+#[tracing::instrument(
+    name = "Adding a new subscriber", skip(form, pool),
+    fields(
+    request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name= %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // let request_id = Uuid::new_v4();
+    // let request_span = tracing::info_span!(
+    //     "Adding a new subscriber.",
+    //     %request_id,
+    //     subscriber_email = %form.email,
+    //     subscriber_name= %form.name
+    // );
+    // let _request_span_guard = request_span.enter();
+    let _query_span = tracing::info_span!("Saving new subscriber details in the database");
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+    "#,
         Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
