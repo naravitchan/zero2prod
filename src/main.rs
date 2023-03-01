e49e95a0d0adfe7e9bd14e1b893bb2fc6f150748
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-03-01 17:20:56 +0700

    [5] - live

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -11,13 +10,14 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    // let connection_pool =
+    //     PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+    //         .expect("Failed to connect to Postgres.");
 
-    // let connection_pool = PgPoolOptions::new()
-    //     .connect_timeout(std::time::Duration::from_secs(2))
-    //     .connect_lazy(&configuration.database.connection_string());
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        // `connect_lazy_with` instead of `connect_lazy`
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
