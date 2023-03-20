6046fa64f4d2dc60d4c1c8bbd503b13e20d68a73
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-03-20 10:34:42 +0700

    [10]  - login - 325

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
+use crate::routes::home;
 use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 
 use actix_web::dev::Server;
@@ -56,6 +57,7 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
